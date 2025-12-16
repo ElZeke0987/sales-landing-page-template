4d41914cc5b3778c52f9fbffc3521b632a511d27
sebastian teran 2025 <eliassebastiansegatori@gmail.com>
2025-12-16 13:24:05 -0300

    Added filter saving system (it doesnt show nowhere, only in is json file)

--- src/app/api/add-category/route.js
+++ src/app/api/add-category/route.js
@@ -1,7 +1,7 @@
 import { addCategory } from "../../../serverMods/modFilters";
 export async function POST(request) {
     const body = await request.json();
-    const { name } = body;
-    await addCategory(name)
-    return new Response(JSON.stringify({ name }));
+    const { val, title } = body;
+    await addCategory({val, title, act: false})
+    return new Response(JSON.stringify({ val, title, act: false }));
 }
--- src/app/shop-managment/comps/dev-panel/filter-list/modComps/addCategory.js
+++ src/app/shop-managment/comps/dev-panel/filter-list/modComps/addCategory.js
@@ -1,39 +1,45 @@
 "use client"
 import { useState } from "react";
-export default function AddFilter() {
-    const [name, setName] = useState('');
-        const [isAdding, setIsAdding] = useState(false);
-        function handleNameChange(event) {
-            setName(event.target.value);
-        }
-        async function addFilter(){
-            const response = await fetch('/api/add-category', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    name,
-                }),
-            }); 
-            const data = await response.json();
-            setIsAdding(false);
-        }
+export default function AddCategory() {
+    const [title, setTitle] = useState('');
+    const [val, setVal] = useState('');
+    const [isAdding, setIsAdding] = useState(false);
+    function handleTitleChange(event) {
+        setTitle(event.target.value);
+    }
+    function handleValChange(event) {
+        setVal(event.target.value);
+    }
+    async function addFilter(){
+        const response = await fetch('/api/add-category', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                val,
+            }),
+        }); 
+        const data = await response.json();
+        setIsAdding(false);
+    }
+    
+    return <div className="dev-panel-add-product">
         
-        return <div className="dev-panel-add-product">
+        {isAdding&&<>
+            <input type="text" placeholder="Title text" value={title} onChange={handleTitleChange}/>
+            <input type="text" placeholder="Value for category (No spaces)" value={val} onChange={handleValChange}/>
+        </>}
+        <div className="dev-panel-add-product-buttons">
+            {isAdding?
+            <>
+            <button className="dev-panel-button cancel-button" onClick={()=>setIsAdding(!isAdding)}>Cancel</button>
+            <button className="dev-panel-button" onClick={addFilter}>Add</button>
             
-            {isAdding&&<>
-                <input type="text" placeholder="Name" value={name} onChange={handleNameChange}/>
-            </>}
-            <div className="dev-panel-add-product-buttons">
-                {isAdding?
-                <>
-                <button className="dev-panel-button cancel-button" onClick={()=>setIsAdding(!isAdding)}>Cancel</button>
-                <button className="dev-panel-button" onClick={addFilter}>Add</button>
-                
-                </>:
-                <button className="dev-panel-button" onClick={()=>setIsAdding(!isAdding)}>Add Filter</button>
-                }
-            </div>
-        </div>;
+            </>:
+            <button className="dev-panel-button" onClick={()=>setIsAdding(!isAdding)}>Add Filter</button>
+            }
+        </div>
+    </div>;
 }
