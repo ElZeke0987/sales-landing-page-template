d96f125a1737b480dccad8173953b2dd22e959c5
sebastian teran 2025 <eliassebastiansegatori@gmail.com>
2025-12-27 14:12:03 -0300

    x

--- backupUntracked/src/app/comps/reusable/cta/CTA.tsx
+++ backupUntracked/src/app/comps/reusable/cta/CTA.tsx
@@ -1,6 +1,6 @@
 import "./cta.scss";
 
-export default function CTA({text= "¡Comienza Ahora!", href="#product-section", ctaStyles}: CTAProps){
+export default function CTA({text= "¡Comienza Ahora!", href="#product-section", ctaStyles}){
     return (
         <div className="flex justify-center ">
             <a href={href} className=" text-white px-6 py-3 rounded-lg shadow  hover:bg-blue-600 transition duration-300 cta-button flex justify-center items-center">
