9e1697f1ade01427569380e82f12c146ef29591d
sebastian teran 2025 <eliassebastiansegatori@gmail.com>
2025-12-27 14:07:25 -0300

    Untyping cta button

--- src/app/comps/reusable/cta/CTA.js
+++ src/app/comps/reusable/cta/CTA.js
@@ -1,7 +1,7 @@
 import "./cta.scss";
-import {CTAProps} from "./types"; 
 
-export default function CTA({text= "¡Comienza Ahora!", href="#product-section", ctaStyles}: CTAProps){
+
+export default function CTA({text= "¡Comienza Ahora!", href="#product-section", ctaStyles}){
     return (
         <div className="flex justify-center ">
             <a href={href} className=" text-white px-6 py-3 rounded-lg shadow  hover:bg-blue-600 transition duration-300 cta-button flex justify-center items-center">
