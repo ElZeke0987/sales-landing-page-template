7577711a18bd8711858e0798ad77be33c7a97a75
sebastian teran 2025 <eliassebastiansegatori@gmail.com>
2026-01-25 13:31:02 -0300

    fixed outstanding products dynamic

--- src/app/comps/epic-outstanding-prods/comps/outsProdsList.js
+++ src/app/comps/epic-outstanding-prods/comps/outsProdsList.js
@@ -15,19 +15,19 @@ export default function OutsProdsList(){
     }
 
     return(
-        <div className="outs-prods-list flex w-full justify-center"> 
+        <div className="outs-prods-list flex justify-center"> 
             {outStandingProducts.map((obj, ind) => {
                 return (
                     <div key={ind} className="out-prod-item bg-center bg-no-repeat bg-cover flex justify-center relative "  >
-                        <div className={"rotation-handler absolute bottom-0 left-0 overflow-hidden rotation-handler-" + ind }>
-                            <div className={"floating-image absolute bottom-0 left-0 overflow-hidden floating-image-" + ind } >
+                        <div className={"rotation-handler overflow-hidden rotation-handler-" + ind }>
+                            <div className={"floating-image relative overflow-hidden floating-image-" + ind } >
                             
                                 <Image src={obj.imgUrl} alt={obj.name} width={60000} height={60000} className="" onLoad={handleImageLoad} />
                             
                             </div>
-                            <div className={"interactuable-part absolute bottom-0 left-0 flex " } onMouseEnter={handleHoverBehindImgs}>
-                                <div className="inside-content flex items-center justify-center">
-                                    <button className="bg-white text-black px-4 py-2 rounded">Ver más</button>
+                            <div className={"interactuable-part absolute bottom-0 left-0 flex justify-center items-end" } onMouseEnter={handleHoverBehindImgs}>
+                                <div className="inside-content flex items-center justify-center h-full">
+                                    <button className="bg-white text-black px-4 py-2 rounded  h-full">Ver más</button>
                                 </div>
                             </div>
         
@@ -36,7 +36,7 @@ export default function OutsProdsList(){
                             
                         </div>
                         
-                        <div className="out-prod-name z-50">{obj.name}</div>
+                        <div className="out-prod-name z-50 absolute bottom-0 left-0 w-full h-full flex justify-center items-center">{obj.name}</div>
                     </div>
                 )
             })
