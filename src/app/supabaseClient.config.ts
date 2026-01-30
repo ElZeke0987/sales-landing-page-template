b51586e542cb5d763417780d488baeb353fcb67b
sebastian teran 2025 <eliassebastiansegatori@gmail.com>
2026-01-30 12:14:14 -0300

    Last fixes in supabaseClient

--- src/app/supabaseClient.config.ts
+++ src/app/supabaseClient.config.ts
@@ -2,6 +2,15 @@ import { createClient } from "@supabase/supabase-js";
 import "dotenv/config"
 
 
+if(!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY must be set", {
+        cause: {
+            NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+            NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+        }
+    });
+}
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
